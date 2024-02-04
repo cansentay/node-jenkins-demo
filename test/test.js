713d377d264e75d68092eb9e388619199df1e3c8
Can Şentay <140180359+cansentay@users.noreply.github.com>
2024-02-04 17:49:56 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,18 +1,10 @@
-'use strict';
+var request = require('supertest');
+var app = require('../index.js');
 
-const request = require('supertest');
-const assert = require('assert');
-const app = require('../index.js');
-
-describe('GET /', () => {
-  it('responds with "Hello World"', (done) => {
-    request(app)
-      .get('/')
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        assert.strictEqual(res.text, 'Hello World');
-        done();
-      });
-  });
+describe('GET /', function() {
+    it('respond with hello world', function(done) {
+        request(app)
+            .get('/')
+            .expect('Hello World', done);
+    });
 });
